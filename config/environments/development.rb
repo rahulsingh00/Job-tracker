8122934fed16fe1c6f438d2b2b486d555934ccd0
suman mukherjee <sumanmukherjee03@gmail.com>
2010-04-10 02:26:01 +0530

    Error in sending mails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,6 @@ config.action_controller.perform_caching             = false
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
-  :enable_starttls_auto => true,
   :address => "smtp.gmail.com",
   :port => 587,
   :authentication => :plain,
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,6 @@ config.action_view.cache_template_loading            = true
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
-  :enable_starttls_auto => true,
   :address => "smtp.gmail.com",
   :port => 587,
   :authentication => :plain,
