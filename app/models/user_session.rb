89d58e6bd38a8ce7ecaa3409835035e6e1f215fc
suman mukherjee <sumanmukherjee03@gmail.com>
2010-04-11 01:55:28 +0530

    Error message display beautified

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -3,7 +3,7 @@ class UserSession < Authlogic::Session::Base
   validate :active_or_not?
   
   def active_or_not?
-    errors.add(:base, "You have not yet verified your account") unless attempted_record && attempted_record.active
+    errors.add(:base, "You have not yet activated your account") unless attempted_record && attempted_record.active
   end
 
 end
